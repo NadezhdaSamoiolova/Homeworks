c6510ff48d76f8672d9ededfb592ae7de0e606b2
Nadezhda <S_Nadja@bk.ru>
2022-07-13 15:50:29 +0300

    The first task is corrected

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,20 +1,20 @@
 ﻿// ***Task 1***
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-// Console.WriteLine("Input first number ");
-// int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input first number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
 
-// Console.WriteLine("Input second number ");
-// int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-// if(num1 > num2)
-// {
-//     Console.WriteLine($"Your number is {num1}");
-// }
-// else
-// {
-//     Console.WriteLine($"Your number is {num2}");
-// }
+if(num1 > num2)
+{
+    Console.WriteLine("Your first number("+ num1 +") is bigger than your second number("+ num2+")");
+}
+else
+{
+    Console.WriteLine("Your second number("+ num2 +") is bigger than your first number("+ num1 +")");
+}
 
 // ***Task 2***
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
@@ -54,15 +54,21 @@
 // ***Task 4***
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-Console.WriteLine("Input number ");
-int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number ");
+// int N = Convert.ToInt32(Console.ReadLine());
 
-int current = N * (-1);
+// int current = N * (-1);
 
-while (current <= N) 
-{
-    if (current % 2 == 0)
-    Console.Write(current + " ");
-    current++;
-}
+// while (current <= N) 
+// {
+//     if (current % 2 == 0)
+//     Console.Write(current + " ");
+//     current++;
+// }
+
+
+//***** А теперь то же самое, с использованием метода *****
+// ***Task 1***
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
 
